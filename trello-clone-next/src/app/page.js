81724a8e147f27fa8339ec4570ba82b22adf71af
mandar145 <cmandar41@gmail.com>
2024-10-29 03:51:05 +0000

    drag-drop functionality

--- trello-clone-next/src/app/page.js
+++ trello-clone-next/src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Board from '../components/Board';
 
+
 export default function Home() {
   return (
     <div className={styles.page}>
